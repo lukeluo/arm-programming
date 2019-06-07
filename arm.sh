10359ea3ca95188d8237b6e8330474787697d08e
Luke Luo <luke.jf.luo@gmail.com>
2019-06-08 01:05:53 +0800

    change network device to virtio_net

--- arm.sh
+++ arm.sh
@@ -2,10 +2,10 @@
 qemu-system-arm -M virt,highmem=off \
   -cpu cortex-a15 \
   -m 2048 \
-  -drive file=./arm.raw,format=raw,index=0,if=virtio,media=disk \
+  -blockdev driver=file,node-name=arm.disk,filename=./arm.raw  -device virtio-blk-pci,drive=arm.disk \
   -smp 2  \
   -kernel boot/zImage \
   -initrd boot/initramfs-linux.img \
   -nographic \
-  -netdev tap,id=nd0,ifname=tap0,script=no,downscript=no -device e1000,netdev=nd0  \
--append 'root=PARTUUID="cc88b513-db87-4d01-aa75-267bfd2b53c4" rw'
+  -netdev tap,id=nd0,ifname=tap0,script=no,downscript=no,vhost=off -device virtio-net-pci,netdev=nd0 \
+-append 'root=PARTUUID="83aedb0a-e597-4e4a-8e0b-e816732af9b6" rw'
